c2671f4fbb4b03c26e386b2073589a74d52dbed5
Nick <bogdanovichkolia1990@gmail.com>
2025-04-24 16:12:43 +0300

    done constructor

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import './js/api';
+import './js/header';
+import './js/hero';
+import './js/about-me';
+import './js/tech-skills';
+import './js/my-projects';
+import './js/benefits';
+import './js/faq';
+import './js/reviews';
+import './js/work-together';
+import './js/footer';
+console.log('main');
