4f2a84090ac9e757cc31aa25268c52f1deaebed4
Alexander Palilov <prostosanekone@gmail.com>
2025-04-25 13:35:56 +0300

    edit file faq of css,html and js

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,1 +1,13 @@
-console.log('faq');
+import Accordion from 'accordion-js';
+import 'accordion-js/dist/accordion.min.css';
+export const accordion = new Accordion('.accordion-list', {
+  duration: 400,
+  showMultiple: false,
+  beforeOpen: function (currentElement) {
+    currentElement.style.backgroundColor = '#bcdfd1';
+  },
+  beforeClose: function (currentElement) {
+    currentElement.style.backgroundColor = '#fff';
+  },
+});
+accordion.open(0);
