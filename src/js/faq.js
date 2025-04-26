4d329e81ff9e03db1af4ebdf5d18183bd5ce0731
Alexander Palilov <prostosanekone@gmail.com>
2025-04-26 14:39:51 +0300

    fix errors file faq.css

--- src/js/faq.js
+++ src/js/faq.js
@@ -2,11 +2,5 @@ import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
 export const accordion = new Accordion('.accordion-list', {
   duration: 400,
-  beforeOpen: function (currentElement) {
-    currentElement.style.backgroundColor = '#bcdfd1';
-  },
-  beforeClose: function (currentElement) {
-    currentElement.style.backgroundColor = '#F0F0F0';
-  },
 });
 accordion.open(0);
