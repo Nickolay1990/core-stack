0812440d931c903e12e0fe0d0166492c071d4fdb
Nick <bogdanovichkolia1990@gmail.com>
2025-04-27 11:36:00 +0300

    done change theme

--- src/js/sidebar.js
+++ src/js/sidebar.js
@@ -1,18 +1,36 @@
-const button = document
-  .querySelector('.side-button')
-  .addEventListener('click', opensideHandler);
+const sideButton = document.querySelector('.side-button');
+const sidebarForm = document.querySelector('.sidebar-form');
+const html = document.querySelector('html');
+
+sideButton.addEventListener('click', opensideHandler);
+sidebarForm.addEventListener('change', changeColorHandler);
 
 function opensideHandler() {
   this.closest('.sidebar').classList.toggle('hidden-bar');
   this.textContent = this.textContent === '<' ? '>' : '<';
 }
 
-const sidebarForm = document.querySelector('.sidebar-form');
-
-sidebarForm.addEventListener('change', changeColorHandler);
-
 function changeColorHandler() {
   const selected = this.querySelector('input[name="color"]:checked').value;
-  document.querySelector('html').classList = '';
-  document.querySelector('html').classList.add(selected);
+  html.classList = '';
+  html.classList.add(selected);
+  localStorage.setItem('theme', selected);
 }
+
+function initTheme() {
+  const theme = localStorage.getItem('theme') || 'classic';
+  setTheme(theme);
+  setRadio(theme);
+}
+
+function setTheme(theme) {
+  html.classList.add(theme);
+  localStorage.setItem('theme', theme);
+}
+
+function setRadio(theme) {
+  const radio = sidebarForm.querySelector(`[value=${theme}]`);
+  radio.checked = true;
+}
+
+initTheme();
