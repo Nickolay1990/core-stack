d4f05aa6ff390b7a7c0cd43e4dead937cd14a590
Nick <bogdanovichkolia1990@gmail.com>
2025-04-29 14:20:41 +0300

    fix scroll to bottom section

--- src/js/header.js
+++ src/js/header.js
@@ -1,13 +1,34 @@
 const refs = {
   changeModalBtn: document.querySelectorAll('[data-menu-handler]'),
   modal: document.querySelector('[data-menu]'),
+  footerLinks: document.querySelectorAll('[data-scrol-down]'),
 };
+
+console.log(refs.footerLinks);
 
 refs.changeModalBtn.forEach(link => {
   link.addEventListener('click', toggleModal);
 });
 
+refs.footerLinks.forEach(link => {
+  link.addEventListener('click', scrolDown);
+});
+
 function toggleModal() {
   refs.modal.classList.toggle('is-open');
   document.body.classList.toggle('no-scroll');
+}
+
+function scrolDown(event) {
+  event.preventDefault();
+  const section = document.querySelector('#contacts');
+  const sectionBottom = section.offsetTop + section.offsetHeight;
+
+  window.scrollTo({
+    top: sectionBottom - window.innerHeight,
+  });
+
+  if (window.innerWidth < 768) {
+    toggleModal();
+  }
 }
