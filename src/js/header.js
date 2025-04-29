234f32c43240aa446f449921d9e87df079ff9eac
ivshynalarysa <ivshynalarysa@gmail.com>
2025-04-29 09:15:04 +0100

    fix js file for header

--- src/js/header.js
+++ src/js/header.js
@@ -4,10 +4,15 @@ const refs = {
   closeModalBtn: document.querySelector('[data-menu-close]'),
 
   modal: document.querySelector('[data-menu]'),
+  closeModalLinks: document.querySelectorAll('.mobile-nav-link, .mobile-order-link')
 };
 
 refs.openModalBtn.addEventListener('click', toggleModal);
 refs.closeModalBtn.addEventListener('click', toggleModal);
+
+refs.closeModalLinks.forEach(link => {
+  link.addEventListener('click', toggleModal);
+});
 
 function toggleModal() {
   refs.modal.classList.toggle('is-open');
